d8ca68b2b7a81a0f09233d09db19c2c66f008879
ashrasmun <ashrasmun@gmail.com>
2021-09-26 17:38:28 +0200

    Place the bot related code in a class

--- bot.py
+++ bot.py
@@ -1,3 +1,4 @@
+import sys
 import greeting
 
 from twitchio.ext import commands
@@ -7,47 +8,45 @@ from details import config, args
 index = 0
 
 
-def main():
-    print('Reading configuration...')
-    config.load(args.bot_config)
+class AshraBot(commands.Bot):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
 
-    bot = commands.Bot(
-        irc_token        = config.tmi_token,
-        client_id        = config.client_id,
-        nick             = config.bot_nick,
-        prefix           = config.bot_prefix,
-        initial_channels = config.channel,
-    )
-
-    @bot.event
-    async def event_ready():
+    async def event_ready(self):
         """Called once when the bot goes online."""
         print(f'{config.bot_nick} is online!')
-        ws = bot._ws  # allow bot to send messages during this event
+        ws = self._ws  # allow bot to send messages during this event
 
         message = f'/me is online, {greeting.get_a_bop()}'
         await ws.send_privmsg(config.channel[0], message)
 
-    @bot.event
-    async def event_message(context):
+    async def event_message(self, context):
         """Runs every time a message is sent in chat."""
         # TODO:
         # Reset the index when the 30s timer cools down.
 
-        # make sure the bot ignores itself and the streamer
+        # Make sure the bot ignores itself and the streamer
         if context.author.name.lower() == config.bot_nick.lower():
             return
 
-        content = context.content.lower()
+        content       = context.content.lower()
         split_content = content.split(' ')
 
-        if 'mus' in split_content:
-            global index
-            paren = '' if index == 0 else f' ({index})'
-            index = index + 1
-            await context.channel.send(f'mus KEKW{paren}')
+        # Make sure the new message is unique
+        global index
+        paren = '' if index == 0 else f' ({index})'
+        index = index + 1
 
-    async def _print_context(context):
+        if 'mus' in split_content:
+            content = f'mus KEKW{paren}'
+        elif 'corvibot' in split_content:
+            content = f'peepoLove{paren}'
+        else:
+            return
+
+        await context.channel.send(content)
+
+    async def _print_context(self, context):
         print(dir(context))
         print(context.color)
         print(context.author)
@@ -59,9 +58,8 @@ def main():
         print(dir(context.message))
         print(context.prefix)
 
-    @bot.command(name='lurk')
-    async def lurk(context):
-        print('?')
+    @commands.command(name = "lurk")
+    async def lurk_command(self, context):
         """
         TODO:
             1. Figure out why the username is always in lowercase instead of
@@ -72,6 +70,25 @@ def main():
         args.pop(0)
         reason = ' '.join(args) if args else 'your lurking'
         await context.send(f'Enjoy {reason}, {context.author.name} peepoLove')
+
+    @commands.command()
+    async def hello(self, ctx):
+        # Send a hello back!
+        await ctx.send(f'Hello {ctx.author.name}!')
+
+
+def main():
+    print(f'I\'m running using: {sys.executable}')
+    print('Reading configuration...')
+    config.load(args.bot_config)
+
+    bot = AshraBot(
+        irc_token        = config.tmi_token,
+        client_id        = config.client_id,
+        nick             = config.bot_nick,
+        prefix           = config.bot_prefix,
+        initial_channels = config.channel,
+    )
 
     # TODO
     # Add command to ban bots:
